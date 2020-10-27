e8ea13d2ad97eae3c1d25b027b08b5008e9867dc
Ivan <Salik52@yandex.ru>
2020-10-27 15:37:33 +0500

    eda

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ class Player:
         level = 0
         zoo_rating = 0
 
+class FoodType:
+    Grass = ('Grass', 1, 5)
+    Meat = ('Meat', 2, 10)
 
 class AnimalData:
     # type_food_eating = ("Grass", 'Meat')
@@ -12,9 +15,8 @@ class AnimalData:
     # rare = (1, 2, 3, 4, 5)
     # habitat = ('Water', 'Ground')
 
-
-    Tiger = ('Meat', 'Middle', 10, 5, 'Ground')
-    Elephant = ('Grass', 'Big', 5, 3, 'Ground')
+    Tiger = (FoodType.Meat, 'Middle', 10, 5, 'Ground')
+    Elephant = (FoodType.Grass, 'Big', 5, 3, 'Ground')
 
 
 class Animal(AnimalData):
@@ -23,21 +25,16 @@ class Animal(AnimalData):
 
 
 
-class Food:
+
+class Food(FoodType):
     def __init__(self, object):
-        self.hj = object.type_food_eating
+        self.hj = object[0]
+
 
     def str(self):
         str = self.hj
         return str
 
 
-class FoodType:
-    def __init__(self):
-        self.rare = (1, 2)
-        self.type = ('Grass', 'Meat')
-        self.time = (5, 10)
-
-    def str(self, hj):
-        str = f'{self.type[hj]},{self.rare[hj]},{self.time[hj]}'
-        return str
+b = Food(AnimalData.Tiger)
+print(b.str())
