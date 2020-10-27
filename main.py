4f588b40f008f28e52cc1750e7780bc8538cbba5
nstvn <71223295+nstvn@users.noreply.github.com>
2020-10-27 16:40:18 +0500

    add some bullshit

--- main.py
+++ main.py
@@ -41,14 +41,40 @@ class AnimalData:
     # food_amount = (5, 10, 15)
     # rare = (1, 2, 3, 4, 5)
     # habitat = ('Water', 'Ground')
+    Tiger = {'type_food': FoodType.Meat, 'sizeA': 'Middle', 'food_amount': 10, 'rare': 5, 'habitat': 'Ground'}
+    Elephant = {'type_food': FoodType.Grass, 'sizeA': 'Big', 'food_amount': 5, 'rare': 3, 'habitat': 'Ground'}
 
-    Tiger = (FoodType.Meat, 'Middle', 10, 5, 'Ground')
-    Elephant = (FoodType.Grass, 'Big', 5, 3, 'Ground')
+    def __init__(self):
+        self.type_food_eating = 0
+        self.sizeA = ''
+        self.food_amount = 0
+        self.rare = 0
+        self.habitat = ''
 
 
 class Animal(AnimalData):
     def __init__(self):
         pass
+
+
+class Tiger(AnimalData):
+    def __init__(self):
+        super().__init__()
+        self.type_food_eating = FoodType.Meat
+        self.sizeA = 'Middle'
+        self.food_amount = 10
+        self.rare = 5
+        self.habitat = 'Ground'
+
+
+class Elephant(AnimalData):
+    def __init__(self):
+        super().__init__()
+        self.type_food_eating = FoodType.Grass
+        self.sizeA = 'Big'
+        self.food_amount = 5
+        self.rare = 3
+        self.habitat = 'Ground'
 
 
 class Food(FoodType):
@@ -94,8 +120,7 @@ class Volier(typeVolier):
                 return typeVolier.bigVolierWater
 
     def put_animal(self, object):
-        if object[1] ==
-
+        pass
 
 
 
